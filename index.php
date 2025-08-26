d61b7b43fb78ac5b610a80e6d3428a1ea7c8e47f
Mac1618 <ma46099@gmail.com>
2025-08-26 21:18:50 +0800

    feat: leared about variables, contionals and concatenations

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sample</title>
+
+  <style>
+    body {
+      display: grid;
+      place-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: sans-serif;
+    }
+  </style>
 </head>
+
 <body>
-  <h1>butikeee</h1>
-  <?php 
-    echo "Bobo ka ba?"
+
+  <?php
+  $book = "Dark Matter";
+  $read = true;
+
+  if ($read) {
+    $message = "You have read $book";
+  } else {
+    $message = "You have NOT read $book";
+  }
   ?>
+  <h1> <?= $message; ?> </h1>
+
 </body>
+
 </html>
