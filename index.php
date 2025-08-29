153e04dc36eaa9823c4b12b1ee34a3267ff91692
Mark Bongulto <mac46099@gmail.com>
2025-08-29 14:19:52 +0800

    feat: learned about functions with signatures, lambda functions, and php built-in function for arrays

--- index.php
+++ index.php
@@ -38,28 +38,54 @@
       [
         'name' => 'The Langoliers',
         'author' => 'Andy Weir',
-        'releaseYear' => 2005,
+        'releaseYear' => 1995,
         'url' => 'http.sample.com'
       ]
     ];
 
     // functions in php
     function filterByAuthor ($books, $author) {
+      $filteredBooks = [];
+
       foreach($books as $book){
         if ($book['author'] === $author) {
-          return [$book];
-        } else {
-          return;
+          $filteredBooks[] = $book;
         }
+
+        return $filteredBooks;
       }
     }
 
+    // Lambda Functions: converting to functions to be generic
+    $filter = function($items, $func) {
+      $filterItems = [];
+
+      foreach($items as $item) {
+        if($func($item)){
+          $filterItems[] = $item;
+        }
+      }
+
+      return $filterItems;
+    };
+
+    // moving the logic here to easily manipulate
+    $lambdaFilterBooks = $filter($books, function ($books) {
+      // logic: equals to 2000 and ABOVE
+      return $books['releaseYear'] <= 2000;
+    });
+
+    // php built-in function for ARRAY
+    $lambdaPhpBuiltIn = array_filter($books, function ($books) {
+      // logic: equals to 2000 and BELOW
+      return $books['releaseYear'] >= 2000;
+    });
   ?>
 
   <!-- with associative arrays -->
   <h1>Recommeded Books:</h1>
   <ol>
-    <?php foreach (filterByAuthor($books, "John Doe") as $book) : ?>
+    <?php foreach ($lambdaFilterBooks as $book) : ?>
       <li>
         <a href="<?= $book['url'] ?>">
           <?= $book['name'] ?> - By <?= $book['author'] ?>
